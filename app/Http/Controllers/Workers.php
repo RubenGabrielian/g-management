e0a4442ef7fc737dfe51f33f98676cbb8f4d7df8
rubengabriel <ruben.gabrielyan@volo.global>
2021-11-01 18:49:31 +0400

    try to get builders salary

--- app/Http/Controllers/Workers.php
+++ app/Http/Controllers/Workers.php
@@ -52,8 +52,12 @@ class Workers extends Controller
         $temp_array = array();
         $i = 0;
         $key_array = array();
+        $temp_array_builders = array();
+        $j = 0;
+        $key_array_builders = array();
 
-        $allWorkers = Salary::where("month", 10)->where("year", 2021)->with('worker')->get();
+
+        $allWorkers = Salary::where("month", 10)->where("year", 2021)->with('worker')->with("work")->get();
         foreach ($allWorkers as $worker) {
             if ($worker->worker->position_id == 1) {
                 array_push($workers, $worker);
@@ -67,16 +71,30 @@ class Workers extends Controller
             $worker['count'] = count($banvorSalary);
             $worker['month_salary'] = $worker->count * $worker->worker->default_salary;
 
-            if(!in_array($worker["worker_id"], $key_array)) {
+            if (!in_array($worker["worker_id"], $key_array)) {
                 $key_array[$i] = $worker["worker_id"];
                 $temp_array[$i] = $worker;
             }
             $i++;
         }
 
-      
 
+        foreach ($builders as $builder) {
 
+            $builder['qm_salary_work'] = $builder->work->qm_price * $builder->qm;
+
+            if (!in_array($builder["worker_id"], $key_array)) {
+                $key_array_builders[$j] = $builder["worker_id"];
+                $temp_array_builders[$j] = $builder;
+            }
+            $j++;
+        }
+
+        foreach($temp_array_builders as $val) {
+            $val['total_month_salary'] = $val->qm_salary_work += $val->qm_salary_work;
+        }
+
+        return $temp_array_builders;
 
 ////        $totals = Salary::where("month", 10)->where("year", 2021)->with('worker')->select("price")->groupBy("worker_id")->get();
 //
